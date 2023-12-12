9c259434b2dfca0a4ebd604698c64056b0e43378
markjericho <jerichodon1998@gmail.com>
2023-12-12 19:03:40 +0800

    fix persist user bug

--- src/controllers/registration_and_authentication_controller/persist_user_controller.ts
+++ src/controllers/registration_and_authentication_controller/persist_user_controller.ts
@@ -1,6 +1,5 @@
 // imported packages
-import jwt, { DecodeOptions } from "jsonwebtoken";
-import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 // imported exported packages
 const accessTokenKey = process.env.ACCESS_TOKEN_KEY;
 import User from "../../models/User";
@@ -17,18 +16,7 @@ export const persistUserController: RequestHandler = async (
 ) => {
 	// this get token code for frontend
 	const accessToken = req.cookies.Bearer;
-	let decoded;
-
-	try {
-		if (!accessTokenKey) {
-			return res.status(500).json("Internal Server Error");
-		} else {
-			decoded = jwt.decode(accessToken, { complete: true } as DecodeOptions);
-		}
-	} catch (err) {
-		// err
-		return res.status(401).json(err);
-	}
+	const decoded = jwt.verify(accessToken, accessTokenKey) as IToken;
 
 	await User.findById(decoded.id)
 		.select("-password")
