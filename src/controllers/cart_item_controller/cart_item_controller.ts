4a3b4706702093977b11240fc1fc05a24f8afc54
markjericho <jerichodon1998@gmail.com>
2023-12-12 16:52:22 +0800

    cart item controller integrate with ts

---
+++ src/controllers/cart_item_controller/cart_item_controller.ts
@@ -0,0 +1,27 @@
+import { Request, RequestHandler, Response } from "express";
+import { isValidObjectId } from "../../helper/isValidObjectId";
+import CartItem from "../../models/CartItem";
+
+export interface ICartReqParam {
+	id: string;
+}
+
+export const getCartItemController: RequestHandler<ICartReqParam> = async (
+	req: Request<ICartReqParam>,
+	res: Response
+) => {
+	const { id } = req.params;
+	isValidObjectId(id, res);
+
+	await CartItem.findById(id)
+		.then((response) => {
+			if (!response) {
+				return res.status(404).json("Nothing found");
+			}
+
+			return res.status(200).json(response);
+		})
+		.catch((error) => {
+			return res.status(500).json("Internal Server error");
+		});
+};
